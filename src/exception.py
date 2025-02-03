736199a90aa8ed73c4e6fac01daee79b482efb74
TM_Thwala <tebzathwala15@gmail.com>
2025-02-03 14:04:38 +0200

    EDA and Problems statement

--- src/exception.py
+++ src/exception.py
@@ -5,11 +5,11 @@ def error_message_detail(error, error_detail:sys):
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
+    )
+    return error_message
         
-        return error_message
-        )
     
-class CustmException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super.__init__(error_message)
         self.error_message=error_message_detail(error_message,error_details=error_detail)
